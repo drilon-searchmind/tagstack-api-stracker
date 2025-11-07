b3204bf4ef6a9c4f600836b6fb2731b367d96a8b
drilon-searchmind <dbr@searchmind.dk>
2025-11-07 09:51:37 +0100

    :bug: re-added puppeteer for serverside gtm detector

--- src/app/api/gtm-scan/route.js
+++ src/app/api/gtm-scan/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 import * as acorn from 'acorn';
-import { ServerSideGTMDetector } from '../../utils/serverSideGTMDetector.js';
+import { ServerSideGTMDetector } from '../../../../utils/serverSideGTMDetector.js';
 
 function extractVarObjectFromScript(code, varName = 'data') {
     if (!code || typeof code !== 'string') return null;
